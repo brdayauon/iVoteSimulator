9b3c31368a6b52049b21335f0a6fcb714bb62cbb
brdayauon <brdayauon@gmail,com>
2020-09-26 15:47:47 -0700

    just pushin

--- iVoteSim/src/SimulationDriver.java
+++ iVoteSim/src/SimulationDriver.java
@@ -1,6 +1,14 @@
 public class SimulationDriver {
 
     public static void main(String[] args){
+
         System.out.println("hello");
+
+
+//        1) create a question type and configure the answers;
+//        2) configure the question for iClicker Service;
+//        3) randomly generate a number students and the answers;
+//        4) submit all the students’ answers to iClicker Service;
+//        5) call the iClicker Service output function to display the result
     }
 }
